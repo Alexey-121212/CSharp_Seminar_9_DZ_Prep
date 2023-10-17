17b330e066382922fd73f7726a074024ea11a4b2
Alexey <alexmail121212@gmail.com>
2023-10-17 18:07:08 +0300

    EX_69 done

--- EX_69/Program.cs
+++ EX_69/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выводим возводим M в степень N рекурсией
+
+Console.Clear();
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m<1) return;
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<1) return;
+
+Console.WriteLine("M in N degree: "+CalcMInNDegree(m,n));
+
+// Methods
+
+static int CalcMInNDegree(int mi, int ni)
+{
+    if (ni>=1)
+    {
+        return mi*CalcMInNDegree(mi,ni-1);
+    }
+    else
+    return 1;
+}
