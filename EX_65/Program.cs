f0814ba4bd7ca583e930474a9ac9a013cf991ba8
Alexey <alexmail121212@gmail.com>
2023-10-17 16:35:33 +0300

    EX_65 done

---
+++ EX_65/Program.cs
@@ -0,0 +1,23 @@
+﻿// Выводим рекурсивным методом натуральные числа от M до N
+
+Console.Clear();
+Console.WriteLine("Введите число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m<1) return;
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<1) return;
+
+PrintSum(m,n);
+
+// Methods
+
+static void PrintSum(int mi, int ni)
+{
+    if (mi>=ni)
+    {
+        PrintSum(mi - 1, ni);
+        Console.WriteLine(mi);
+    }
+}
