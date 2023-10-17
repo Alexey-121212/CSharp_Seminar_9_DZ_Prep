5713afd095de9e9fb7aa167b340d2aa13c2e7115
Alexey <alexmail121212@gmail.com>
2023-10-17 17:53:05 +0300

    EX_67 done

---
+++ EX_63_1_orig/Program.cs
@@ -0,0 +1,20 @@
+﻿// Выводим рекурсивным методом числа от 1 до N
+
+
+Console.Clear();
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<1) return;
+
+PrintSum(n);
+
+// Methods
+
+static void PrintSum(int n)
+{
+    if (n>0)
+    {
+        PrintSum(n - 1);
+        Console.WriteLine(n);
+    }
+}
---
+++ EX_67/Program.cs
@@ -0,0 +1,28 @@
+﻿// Находим рекурсивным методом сумму цифр числа N
+
+Console.Clear();
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 1)
+{
+    Console.WriteLine("Не то число, введите заново");
+    return;
+}
+
+Console.WriteLine(CalcDigSum(n));
+
+// Methods
+
+static int CalcDigSum(int n)
+{
+    if (n > 0)
+    {
+
+        return n % 10 + CalcDigSum(n / 10);
+    }
+    else
+    {
+        return 0;
+    }
+}
