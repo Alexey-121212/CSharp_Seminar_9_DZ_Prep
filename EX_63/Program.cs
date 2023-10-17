258289f115d630fc820d999b4bd1c408304025d6
Alexey <alexmail121212@gmail.com>
2023-10-17 15:53:23 +0300

    EX 63 done

---
+++ EX_63/Program.cs
@@ -0,0 +1,22 @@
+﻿// Находим рекурсивным методом сумм чисел от 1 до namespace Name
+
+Console.Clear();
+
+Console.WriteLine("Введите число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CalcSum(n));
+
+// Methods
+
+static int CalcSum(int n)
+{
+    if (n>0)
+    {
+        return n+CalcSum(n - 1);
+    }
+    else
+    {
+        return 0;
+    }
+}
