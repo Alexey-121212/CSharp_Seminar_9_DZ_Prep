bce33eadb97742e95a4bfe0a4a7f64d112db847b
Alexey <alexmail121212@gmail.com>
2023-10-25 20:59:02 +0300

    ff

--- EX_MY_2_sortByChoiceRecurcion/Program.cs
+++ EX_MY_2_sortByChoiceRecurcion/Program.cs
@@ -8,6 +8,9 @@ using static ArrayCreator;
 int length = 10;
 int[] massiv = CreateFillRndIntArray(length);
 
+Clear();
+
+
 Console.Clear();
 PrintRndIntArray(massiv, 0);
 Console.WriteLine();
@@ -15,6 +18,8 @@ Console.WriteLine();
 
 SortArrayChoice(massiv, 5);
 PrintRndIntArray(massiv, 0);
+
+
 
 
 
